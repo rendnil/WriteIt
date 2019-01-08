c6b6f2309e092b27ca7e431979128a48fe11c8b5
rendnil <andrew.lindner.15@gmail.com>
2019-01-08 17:13:34 -0500

    added post item infra

---
+++ src/components/AllPostsPage/PostItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+class PostItem extends React.Component{
+  render(){
+    return(
+      <li>Post Item</li>
+    )
+  }
+}
+
+export default PostItem
--- src/components/AllPostsPage/PostsList.js
+++ src/components/AllPostsPage/PostsList.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { connect } from 'react-redux'
+import PostItem from "./PostItem"
 
-const PostsList = (props) => {
+const PostsList = ({posts}) => {
   return(
+    <div>
     <h2> Posts List </h2>
-    
+    {posts.map((post)=>{
+      return <PostItem />
+    })}
+    </div>
   )
 }
 
