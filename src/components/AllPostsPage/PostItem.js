baae842226db18aa8ca6f130fb738340e2dd1add
rendnil <andrew.lindner.15@gmail.com>
2019-01-10 18:56:39 -0500

    added rough list item

--- src/components/AllPostsPage/PostItem.js
+++ src/components/AllPostsPage/PostItem.js
@@ -7,6 +7,14 @@ class PostItem extends React.Component{
       <div style={{borderStyle: "solid", borderWidth:"2px", marginBottom:"2%"}}>
       <li>{this.props.post.title}</li>
       <li style={{position:"relative", left:"10%"}}>{this.props.post.content}</li>
+      <li style={{position:"relative", left:"20%"}}>Upvotes: {this.props.post.upvotes}</li>
+      <li style={{position:"relative", left:"20%"}}>Downvotes: {this.props.post.downvotes}</li>
+      <li style={{position:"relative", left:"20%"}}>Net: {this.props.post.upvotes+this.props.post.downvotes} </li>
+
+      <button>Up</button>
+      <button>Down</button>
+
+
       </div>
     )
   }
