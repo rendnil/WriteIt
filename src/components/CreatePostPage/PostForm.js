de932ed76dcd33d409a6a24a0c06352e48018cb5
rendnil <andrew.lindner.15@gmail.com>
2019-01-14 15:06:41 -0500

    added controlled form

--- src/components/CreatePostPage/PostForm.js
+++ src/components/CreatePostPage/PostForm.js
@@ -2,17 +2,31 @@ import React from "react"
 
 
 class PostForm extends React.Component{
+  state = {
+    title: "",
+    content: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]:event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
   render(){
     return(
       <div>
       <h1>Post Form</h1>
-      <form>
-      <input id="postTitle" type = "text" placeholder="Title"/>
-      <br/>
-      <textarea id="postContent" rows="10" cols="20" placeholder="Content"/>
-      <br/>
-      <input type="submit" value="Submit"/>
-      </form>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.title} name="title"id="postTitle" type = "text" placeholder="Title"/>
+          <br/>
+          <textarea onChange={this.handleChange} name="content" id="postContent" rows="10" cols="20" placeholder="Content"/>
+          <br/>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     )
   }
