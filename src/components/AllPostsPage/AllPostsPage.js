59b9d08ec2dfc50d757ff7050a3dc3703a30991b
rendnil <andrew.lindner.15@gmail.com>
2019-01-15 14:13:33 -0500

    material-ui infra

--- src/components/AllPostsPage/AllPostsPage.js
+++ src/components/AllPostsPage/AllPostsPage.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PostsList from "./PostsList"
+import Button from '@material-ui/core/Button';
 
 const AllPostsPage = () => {
   return (
     <div>
+      <Button variant="contained" color="warninga">hey</Button>
       <h1>All Posts</h1>
       <PostsList />
 
