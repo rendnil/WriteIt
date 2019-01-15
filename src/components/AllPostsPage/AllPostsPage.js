4abefc0717b1c765e5620c3d3d67c4f73073d384
rendnil <andrew.lindner.15@gmail.com>
2019-01-15 15:12:05 -0500

    updated navbar

--- src/components/AllPostsPage/AllPostsPage.js
+++ src/components/AllPostsPage/AllPostsPage.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 const AllPostsPage = () => {
   return (
     <div>
-      <Button variant="contained" color="warninga">hey</Button>
+      
       <h1>All Posts</h1>
       <PostsList />
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,23 @@
 import React from "react"
 import {NavLink} from 'react-router-dom';
+import {AppBar, Button, Toolbar, Typography} from '@material-ui/core';
 
 
 const NavBar = () => {
   return(
     <div>
-    NavBar<br/>
-    <NavLink to = "/">Posts</NavLink><br/>
-    <NavLink to = "/new">Create New Post</NavLink>
+    <AppBar  position="static">
+    <Toolbar>
+    <Typography style={{marginRight:"5%"}} variant = "h4"color="inherit">WriteIt</Typography>
+
+    <Button color="inherit">
+      <NavLink style={{textDecoration:"none", color:"inherit"}}to = "/">Posts</NavLink>
+    </Button>
+    <Button color="inherit">
+      <NavLink style={{textDecoration:"none", color:"inherit"}}to = "/new">Create Post</NavLink>
+    </Button>
+    </Toolbar>
+    </AppBar>
 
     </div>
   )
