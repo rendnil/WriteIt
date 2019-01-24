09ab919d86ce53e526eb625b9c27e71168b71aa0
rendnil <andrew.lindner.15@gmail.com>
2019-01-23 19:20:41 -0500

    added enzyme testing infra

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {shallow} from 'enzyme'
+import App from "../App"
+import NavBar from "../NavBar"
+
+let wrapped
+
+beforeEach(()=>{
+  wrapped = shallow(<App />)
+})
+
+
+it(`has a navbar`,() => {
+  expect(wrapped.find(NavBar).length).toEqual(1)
+})
