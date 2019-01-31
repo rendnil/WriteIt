ea804e9cbced8b4dd31a37a70621995eda31e86b
rendnil <andrew.lindner.15@gmail.com>
2019-01-31 14:08:19 -0500

    add createComment method to adapter

--- src/apis/CommentsAdapter.js
+++ src/apis/CommentsAdapter.js
@@ -8,6 +8,26 @@ export default class CommentsAdapter{
     )
   }
 
+  static createComment(postId, commenterId, content){
+    return(
+      fetch(`${API_BASE_URL}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          comment:{
+            post_id: postId,
+            commenter_id: commenterId,
+            content: content
+          }
+        })
+      })//end fetch
+      .then(r=>r.json())
+    )
+  }
+
 
 
 }
--- src/components/PostPage/PostPage.js
+++ src/components/PostPage/PostPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import withAuth from "../../hocs/withAuth"
 import PostContent from "./PostContent"
 import CommentsContainer from "./CommentsContainer"
 
