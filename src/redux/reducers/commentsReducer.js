1c92c97f70fb5055113f9aa2d2453bea6c7f0e91
rendnil <andrew.lindner.15@gmail.com>
2019-02-01 15:29:44 -0500

    add create_comment type to reducer

--- src/redux/reducers/commentsReducer.js
+++ src/redux/reducers/commentsReducer.js
@@ -1,10 +1,12 @@
 const defaultState = []
 
 export default function(state=defaultState, action){
-  
+
   switch(action.type){
     case "FETCH_COMMENTS":
       return action.payload
+    case "CREATE_COMMENT":
+      return [...state, action.payload]
 
     default:
       return state
