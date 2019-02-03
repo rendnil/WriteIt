507767ba834d0d815f69aa7193fc4802340aa588
rendnil <andrew.lindner.15@gmail.com>
2019-02-03 13:34:27 -0500

    select post test

--- src/redux/actions/__tests__/Actions.test.js
+++ src/redux/actions/__tests__/Actions.test.js
@@ -3,6 +3,12 @@ import {selectPost} from "../selectPostAction"
 
 describe('selectPost',() => {
   it(`has the correct type`,() => {
-    const action = selectPost()
+    const action = selectPost(7)
+    expect(action.type).toEqual("SELECT_POST")
+  })
+
+  it('has the correct payload', () => {
+    const action = selectPost(7)
+    expect(action.payload).toEqual(7)
   })
 })
