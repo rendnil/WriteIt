3acc97309f7cacfb733d2311e65dc10e6c15850a
rendnil <andrew.lindner.15@gmail.com>
2019-02-04 13:47:35 -0500

    updated create post and fetch post tests

--- src/redux/reducers/__tests__/posts.test.js
+++ src/redux/reducers/__tests__/posts.test.js
@@ -1,5 +1,10 @@
 import postsReducer from "../postsReducer"
 
+const defaultState = {
+  posts: [],
+  selectedPost: null
+}
+
 it('handles actions with unknown type',() => {
   const action = {
     type: "some random type"
@@ -7,4 +12,31 @@ it('handles actions with unknown type',() => {
 
   const newState = postsReducer([], action)
   expect(newState).toEqual([])
+})
+
+it('handles actions of type FETCH_ALL_POSTS',() => {
+  const action = {
+    type: "FETCH_ALL_POSTS",
+    payload: ['Post1', 'Post2', 'Post3']
+  }
+
+  const newState = postsReducer(defaultState, action)
+  expect(newState).toEqual(
+    {selectedPost: null,
+      posts:['Post1', 'Post2', 'Post3']
+    })
+})
+
+it('handles actions of type CREATE_POST',() => {
+  const action = {
+    type: "CREATE_POST",
+    payload: 'Post4'
+  }
+
+  const newState = postsReducer({selectedPost: null, posts:['Post1', 'Post2', 'Post3']}, action)
+  expect(newState).toEqual(
+    {selectedPost: null, 
+      posts: ['Post1', 'Post2', 'Post3','Post4']
+  })
+
 })
