2cdb5c7d428e1b7ec11ecfd92e2d1fb8809866d6
rendnil <andrew.lindner.15@gmail.com>
2019-02-04 13:17:45 -0500

    additional comment reducer tests

--- src/redux/reducers/__tests__/comments.test.js
+++ src/redux/reducers/__tests__/comments.test.js
@@ -10,3 +10,23 @@ it('handles actions of type FETCH_COMMENTS',() => {
   expect(newState).toEqual(['Comment1', 'Comment2', 'Comment3'])
 
 })
+
+it('handles actions of type CREATE_COMMENT',() => {
+  const action = {
+    type: "CREATE_COMMENT",
+    payload: "Comment4"
+  }
+
+  const oldState = ['Comment1', 'Comment2', 'Comment3']
+  const newState = commentsReducer(oldState, action)
+  expect(newState).toEqual(['Comment1', 'Comment2', 'Comment3', 'Comment4'])
+})
+
+it('handles actions with unknown type',() => {
+  const action = {
+    type: "some random type"
+  }
+
+  const newState = commentsReducer([], action)
+  expect(newState).toEqual([])
+})
