2db6a281c558b8624b375bae1613f7423ac3985e
rendnil <andrew.lindner.15@gmail.com>
2019-02-04 14:47:43 -0500

    select post test

--- src/redux/reducers/__tests__/posts.test.js
+++ src/redux/reducers/__tests__/posts.test.js
@@ -33,10 +33,77 @@ it('handles actions of type CREATE_POST',() => {
     payload: 'Post4'
   }
 
-  const newState = postsReducer({selectedPost: null, posts:['Post1', 'Post2', 'Post3']}, action)
+  const oldState = {selectedPost: null, posts:['Post1', 'Post2', 'Post3']}
+  const newState = postsReducer(oldState, action)
   expect(newState).toEqual(
-    {selectedPost: null, 
+    {selectedPost: null,
       posts: ['Post1', 'Post2', 'Post3','Post4']
   })
+
+})
+
+it('handles actions of type UPDATE_UPVOTES',() => {
+  const action = {
+    type:"UPDATE_UPVOTES",
+    payload:{
+      post: {id:3, title:"Post3 Title", content:"Post3 Content",
+            upvotes:7, downvotes:3, author_id: 7
+      }
+    }
+  }
+
+  const oldState = {
+    selectedPost: null,
+    posts:[
+      {id:1, title:"Post1 Title", content:"Post1 Content", upvotes:2, downvotes:1, author_id:5},
+      {id:2, title:"Post2 Title", content:"Post2 Content", upvotes:4, downvotes:2, author_id:6},
+      {id:3, title:"Post3 Title", content:"Post3 Content", upvotes:6, downvotes:3, author_id:7},
+      {id:4, title:"Post4 Title", content:"Post4 Content", upvotes:8, downvotes:4, author_id:8}
+    ]
+  }
+
+  const newState = postsReducer(oldState, action)
+  expect(newState).toEqual(
+    {
+      selectedPost: null,
+      posts:[
+        {id:1, title:"Post1 Title", content:"Post1 Content", upvotes:2, downvotes:1, author_id:5},
+        {id:2, title:"Post2 Title", content:"Post2 Content", upvotes:4, downvotes:2, author_id:6},
+        {id:4, title:"Post4 Title", content:"Post4 Content", upvotes:8, downvotes:4, author_id:8},
+        {id:3, title:"Post3 Title", content:"Post3 Content", upvotes:7, downvotes:3, author_id:7}
+      ]
+    }
+  )
+
+})
+
+it('handles actions of type SELECT_POST',() => {
+  const action = {
+    type:"SELECT_POST",
+    payload:2
+  }
+
+  const oldState = {
+    selectedPost: null,
+    posts:[
+      {id:1, title:"Post1 Title", content:"Post1 Content", upvotes:2, downvotes:1, author_id:5},
+      {id:2, title:"Post2 Title", content:"Post2 Content", upvotes:7, downvotes:2, author_id:7},
+      {id:3, title:"Post3 Title", content:"Post3 Content", upvotes:6, downvotes:3, author_id:7},
+      {id:4, title:"Post4 Title", content:"Post4 Content", upvotes:8, downvotes:4, author_id:8}
+    ]
+  }
+
+  const newState = postsReducer(oldState, action)
+  expect(newState).toEqual(
+    {
+      posts:[
+        {id:1, title:"Post1 Title", content:"Post1 Content", upvotes:2, downvotes:1, author_id:5},
+        {id:2, title:"Post2 Title", content:"Post2 Content", upvotes:7, downvotes:2, author_id:7},
+        {id:3, title:"Post3 Title", content:"Post3 Content", upvotes:6, downvotes:3, author_id:7},
+        {id:4, title:"Post4 Title", content:"Post4 Content", upvotes:8, downvotes:4, author_id:8}
+      ],
+      selectedPost: {id:2, title:"Post2 Title", content:"Post2 Content", upvotes:7, downvotes:2, author_id: 7}
+    }
+  )
 
 })
