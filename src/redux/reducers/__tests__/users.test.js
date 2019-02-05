65e2e3f4d3ba2c9f9041f7d9ed7f117df82f73ea
rendnil <andrew.lindner.15@gmail.com>
2019-02-05 10:10:40 -0500

    additional users reducer testsa

--- src/redux/reducers/__tests__/users.test.js
+++ src/redux/reducers/__tests__/users.test.js
@@ -23,7 +23,40 @@ it('handles actions of type SET_CURRENT_USER',() => {
     payload: {id:5, username:"username5"}
   }
 
-  const newState = usersReducer(defaultState,action)
-  expect(newState)
+  const newState = usersReducer(defaultState, action)
 
+  expect(newState).toEqual({
+    user: {id:5, username: "username5"},
+    loggedIn: true,
+    authenticatingUser: false,
+    failedLogin: false,
+    error: null,
+    failedSignUp: false
+  })
+})
+it('handles actions of type FAILED_LOGIN',() => {
+  const action = {
+    type:'FAILED_LOGIN'
+  }
+
+  const newState = usersReducer(defaultState, action)
+
+  expect(newState).toEqual({
+    user: null,
+    loggedIn: false,
+    authenticatingUser: false,
+    failedLogin: true,
+    error: null,
+    failedSignUp: false
+  })
+})
+
+it('handles actions of type SIGN_OUT',() => {
+  const action = {
+    type:'SIGN_OUT'
+  }
+
+  const newState = usersReducer(defaultState, action)
+
+  expect(newState).toEqual(defaultState)
 })
