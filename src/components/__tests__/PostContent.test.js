24437af2628a4f565dfe43f81b68f88f9c2372c7
rendnil <andrew.lindner.15@gmail.com>
2019-02-10 14:12:51 -0500

    post content component tests

---
+++ src/components/__tests__/PostContent.test.js
@@ -0,0 +1,29 @@
+import React from "react"
+import {shallow} from 'enzyme'
+import Root from "../../Root"
+import PostContent from "../PostPage/PostContent"
+
+
+let wrapped
+
+beforeEach(() => {
+  const initialState = {
+    posts: {
+      selectedPost: {id: 1, author_id: 1, title: "Title1", content: "Content1"}
+    }
+  }
+  wrapped = shallow(
+    <Root initialState={initialState}>
+      <PostContent />
+    </Root>
+  )
+})
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
+it('contains title and content text',() => {
+  expect(wrapped.render().text()).toContain('Title1')
+  expect(wrapped.render().text()).toContain('Content1')
+})
