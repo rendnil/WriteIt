ea0ac0fe2ad692dd323cf7adadbb6d61bd69bbab
rendnil <andrew.lindner.15@gmail.com>
2019-02-12 16:24:36 -0500

    mock fetch infra

--- src/redux/actions/__tests__/Actions.test.js
+++ src/redux/actions/__tests__/Actions.test.js
@@ -1,5 +1,13 @@
 import React from "react"
 import {selectPost} from "../selectPostAction"
+import configureStore from "redux-mock-store"
+import thunk from 'redux-thunk'
+import {fetchPosts} from "../fetchPostsAction"
+import fetchMock from "fetch-mock"
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
 describe('selectPost',() => {
   it(`has the correct type`,() => {
@@ -11,4 +19,31 @@ describe('selectPost',() => {
     const action = selectPost(7)
     expect(action.payload).toEqual(7)
   })
+})
+
+describe('async actions',() => {
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  describe('fetchPosts Action',() => {
+     it('should post and return data',() => {
+
+       fetchMock.getOnce(`${API_BASE_URL}/comments`, {
+     body: { todos: ['do something'] },
+     headers: { 'content-type': 'application/json' }
+   })
+
+         const store = mockStore({})
+
+
+      return store.dispatch(fetchPosts())
+        .then(() => {
+            const actions = store.getActions()
+            expect(actions[0]).toEqual({type:"hey", payload:"hey"})
+          })
+
+         })
+      })
 })
--- src/redux/actions/createCommentAction.js
+++ src/redux/actions/createCommentAction.js
@@ -2,7 +2,7 @@ import CommentsAdapter from "../../apis/CommentsAdapter"
 
 export const postComment = (postId, commenterId, content) => {
   return (dispatch) => {
-    CommentsAdapter.createComment(postId, commenterId, content)
+    return CommentsAdapter.createComment(postId, commenterId, content)
     .then((data)=>{
       dispatch({type:"CREATE_COMMENT", payload: data})
     })
