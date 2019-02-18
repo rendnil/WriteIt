ad4953513556282ca199fc7690bb07ea163cad5c
rendnil <andrew.lindner.15@gmail.com>
2019-02-18 18:24:57 -0500

    update post form inputs styling

--- src/components/CreatePostPage/PostForm.js
+++ src/components/CreatePostPage/PostForm.js
@@ -2,6 +2,17 @@ import React from "react"
 import { connect } from 'react-redux'
 import {createPost} from "../../redux/actions/createPostAction"
 import {Redirect} from "react-router"
+import {TextField} from '@material-ui/core';
+
+// <TextField
+//          id="outlined-name"
+//          label="Name"
+//          className={classes.textField}
+//          value={this.state.name}
+//          onChange={this.handleChange('name')}
+//          margin="normal"
+//          variant="outlined"
+//        />
 
 class PostForm extends React.Component{
   state = {
@@ -24,12 +35,32 @@ class PostForm extends React.Component{
   render(){
     if (!this.state.submitted){
       return(
-        <div>
+        <div style={{maxWidth:"60%", margin:"auto"}}>
         <h1>Post Form</h1>
         <form onSubmit={this.handleSubmit}>
-        <input onChange={this.handleChange} value={this.state.title} name="title"id="postTitle" type = "text" placeholder="Title"/>
+        <TextField
+                name="title"
+                label="Title"
+                value={this.state.title}
+                onChange={this.handleChange}
+                margin="normal"
+                variant="outlined"
+                fullWidth="true"
+              />
+        {/*<input onChange={this.handleChange} value={this.state.title} name="title"id="postTitle" type = "text" placeholder="Title"/> */}
         <br/>
-        <textarea onChange={this.handleChange} value={this.state.content} name="content" id="postContent" rows="10" cols="20" placeholder="Content"/>
+        <TextField
+                name="content"
+                label="Content"
+                value={this.state.content}
+                onChange={this.handleChange}
+                margin="normal"
+                variant="outlined"
+                fullWidth="true"
+                multiline="true"
+                rows="10"
+              />
+        {/*<textarea onChange={this.handleChange} value={this.state.content} name="content" id="postContent" rows="10" cols="20" placeholder="Content"/>*/}
         <br/>
         <input type="submit" value="Submit"/>
         </form>
